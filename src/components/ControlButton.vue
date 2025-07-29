c30ca6cdcb07aae2a49a162a28091b18251501ce
xabbudm <dmuhle@gmail.com>
2025-07-29 15:04:38 +0200

    Create ControlButton.vue

---
+++ src/components/ControlButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button 
+    class="control-button" 
+    :class="isRunning ? 'stop-button' : 'start-button'" 
+    @click="$emit('toggle')"
+  >
+    {{ isRunning ? 'Stop' : 'Start' }}
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  isRunning: {
+    type: Boolean,
+    default: false
+  }
+})
+
+defineEmits(['toggle'])
+</script>
